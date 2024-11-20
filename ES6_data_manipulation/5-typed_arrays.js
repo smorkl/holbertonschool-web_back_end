8af091036bb2d78f6ddab545dbeb7fee636c6861
smorkl <smsalazar0319@gmail.com>
2024-11-20 08:54:03 -0500

    create

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+function createInt8TypedArray(length, position, value) {
+    if (position < 0 || position >= length) {
+      throw new Error('Position outside range');
+    }
+    const buffer = new ArrayBuffer(length);
+    const view = new Int8Array(buffer);
+    view[position] = value;
+    return view;
+}
