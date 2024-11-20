eee516b97c023774b3bca8f25b570a41c28fa73f
smorkl <smsalazar0319@gmail.com>
2024-11-20 09:27:02 -0500

    create

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,12 @@
-function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const newGrade = newGrades.find(grade => grade.studentId === student.id);
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  return arr
+    .filter((el) => el.location === city)
+    .map((person) => {
+      const grades = newGrades.filter((el) => el.studentId === person.id);
+      const grade = grades.length ? grades[0].grade : 'N/A';
       return {
-        ...student,
-        grade: newGrade ? newGrade.grade : 'N/A',
+        ...person,
+        grade,
       };
     });
 }
