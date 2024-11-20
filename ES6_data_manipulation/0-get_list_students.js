70e4f995f5fd54181ee0fe5312cdacc1272da44d
smorkl <smsalazar0319@gmail.com>
2024-11-20 07:51:05 -0500

    cretae

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,8 @@
-function getListStudents() {
+export default function getListStudents() {
     return [
       { id: 1, firstName: "Guillaume", location: "San Francisco" },
       { id: 2, firstName: "James", location: "Columbia" },
       { id: 5, firstName: "Serena", location: "San Francisco" },
     ];
   }
-  
-  // Ejemplo de uso:
-  const students = getListStudents();
-  console.log(students);
   
