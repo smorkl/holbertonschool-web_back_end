dc0b3cea79203f46c84c16ca8af21c93415ffd9f
smorkl <smsalazar0319@gmail.com>
2024-11-20 08:16:01 -0500

    create

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,7 @@
-function getListStudents() {
+export default function getListStudents() {
     return [
       { id: 1, firstName: "Guillaume", location: "San Francisco" },
       { id: 2, firstName: "James", location: "Columbia" },
       { id: 5, firstName: "Serena", location: "San Francisco" },
     ];
   }
-
-const estudiantes = getListStudents();
-console.log(estudiantes);
