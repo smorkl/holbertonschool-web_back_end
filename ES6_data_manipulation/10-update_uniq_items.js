98e1ba56b9ad8ded5c0d42b7f3a9034ddfc90921
smorkl <smsalazar0319@gmail.com>
2024-11-20 09:18:48 -0500

    create

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,6 @@
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) throw Error('Cannot process');
-  const copy = new Map(map); // Create a copy of the original Map
-  for (const [k, v] of copy.entries()) {
-    if (v === 1) {
-    copy.set(k, 100);
-    }
-  }
+  const copy = map;
+  for (const [k, v] of map.entries()) if (v === 1) copy.set(k, 100);
   return copy;
 }
