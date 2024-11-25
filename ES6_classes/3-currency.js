057a293f63769259b7d9058d1a9556bef414cb2f
smorkl <smsalazar0319@gmail.com>
2024-11-25 12:06:15 -0500

    create

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,18 +1,18 @@
-export default class HolbertonCourse {
-  constructor(name, length, students) {
-    if (typeof name !== 'string') throw TypeError('name must be a String');
-    if (typeof length !== 'number') throw TypeError('length must be a Number');
-    if (!Array.isArray(students)) throw TypeError('students must be an Array');
-    for (const el of students) {
-      if (typeof el !== 'string') { throw TypeError('students must contain strings'); }
-    }
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
     this._name = name;
-    this._length = length;
-    this._students = students;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
   }
 
   set name(name) {
-    if (typeof name !== 'string') throw TypeError('name must be a String');
     this._name = name;
   }
 
@@ -20,24 +20,7 @@ export default class HolbertonCourse {
     return this._name;
   }
 
-  set length(length) {
-    if (typeof length !== 'number') throw TypeError('length must be a Number');
-    this._length = length;
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  set students(students) {
-    if (!Array.isArray(students)) throw TypeError('students must be an Array');
-    for (const el of students) {
-      if (typeof el !== 'string') { throw TypeError('students must contain strings'); }
-    }
-    this._students = students;
-  }
-
-  get students() {
-    return this._students;
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
   }
 }
