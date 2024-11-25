3d02108a7533d353b73d9aa9490dd816d2849ce3
smorkl <smsalazar0319@gmail.com>
2024-11-25 11:31:50 -0500

    create

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,4 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-  const arrayConcatenado = array1.concat(array2, string);
-  return arrayConcatenado
+  const arrayConca = [...array1, ...array2, ...string];
+  return arrayConca
 }
