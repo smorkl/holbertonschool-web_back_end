c1aa8d7d09c50d6c9b8bc162ba21b5d592caf359
smorkl <smsalazar0319@gmail.com>
2024-11-25 12:09:07 -0500

    create

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,16 +1,10 @@
-export default class Building {
-  constructor(sqft) {
-    this._sqft = sqft;
-    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
   }
 
-  set sqft(value) {
-    this._sqft = value;
-  }
-
-  get sqft() {
-    return this._sqft;
+  toString() {
+    return `[object ${this._code}]`;
   }
 }
