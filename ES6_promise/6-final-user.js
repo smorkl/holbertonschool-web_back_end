7946d65c161d85ebb07bd27b9f7bdebe6fbdafa5
smorkl <smsalazar0319@gmail.com>
2024-11-26 07:47:25 -0500

    create

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,12 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-  .then((results) => {
-    return results.map((result) => ({
-      status: result.status,
-      value: result.status === "fulfilled" ? result.value : String(result.reason),
-    }));
-  });
+    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      return results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : String(result.reason),
+      }));
+    });
 }
