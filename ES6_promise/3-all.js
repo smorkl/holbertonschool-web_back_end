8cdfd949fc8701eaf7742deb454a223db4cd1ecf
smorkl <smsalazar0319@gmail.com>
2024-11-26 07:12:51 -0500

    create

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,13 +1,13 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((userResult) => {
-      const [{ body}, {firstName, lastName}] = userResult;
+      const [{ body }, { firstName, lastName }] = userResult;
 
       console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch((error) => {
-      console.log("Signup system offline");
+      console.log('Signup system offline');
     });
 }
