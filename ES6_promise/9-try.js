94ee01c5bbb50d2ff8a8764eac76a9c603121e47
smorkl <smsalazar0319@gmail.com>
2024-11-26 08:17:54 -0500

    create

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,3 +1,14 @@
-export function getResponseFromAPI() {
-  return Promise
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(`${error.name}: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
 }
