ae9d97838878ddf88f5d2ac55b2447716c2e0054
smorkl <smsalazar0319@gmail.com>
2024-11-26 07:56:25 -0500

    create

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => {
-      return results.map((result) => (result) ({
+      return results.map((result) => ({
         status: result.status,
         value: result.status === 'fulfilled' ? result.value : String(result.reason),
       }));
