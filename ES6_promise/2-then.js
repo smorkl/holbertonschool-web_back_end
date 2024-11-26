d2188f3677343340532b28f400286a6d208f1575
smorkl <smsalazar0319@gmail.com>
2024-11-26 08:23:02 -0500

    create

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
-      return {status: 200, body: 'success'}
+      return { status: 200, body: 'success' }
     })
     .catch(() => {
       return new Error()
