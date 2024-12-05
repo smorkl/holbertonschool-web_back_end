7299f4cc653264d270c10613c511b46656c53882
smorkl <smsalazar0319@gmail.com>
2024-12-05 13:07:27 -0500

    create

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This module provides a function to create a multiplier function.
+
+The `make_multiplier` function takes a float as input and returns a new function that multiplies its input by the given multiplier.
+"""
+
+def make_multiplier(multiplier: float) -> float:
+    """
+    Creates a multiplier function.
+
+    Args:
+        multiplier: The factor to multiply by.
+
+    Returns:
+        A function that multiplies its input by the given multiplier.
+    """
+    return (multiplier * multiplier)
