beb252c98c6e612db227a82f841d5a6f144b74b7
smorkl <smsalazar0319@gmail.com>
2024-12-04 20:58:51 -0500

    create

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This module concatenates two strings
+
+fuction
+    concat (str1, str2) concatenate 2 strings
+"""
+def concat (str1: str, str2: str) -> str:
+    """
+    concatenate 2 strings
+
+    arg:
+        str1: The first string.
+        str2: The second string.
+    
+    Returns:
+        The concatenation of `str1` and `str2`.    """
+    return str1 + str2
