f0816ddc5c011980571c0443d9535a6079ef2c52
smorkl <smsalazar0319@gmail.com>
2024-12-05 10:54:48 -0500

    create

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+The module converts a float to a string
+
+fuction:
+    to_str(n): converse float a string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    converse float a string
+
+    arg:
+        n: the float number
+
+    Return:
+        a string
+    """
+    return str(n)
