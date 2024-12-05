28df98292324659dc31f7950dc78bba1a16f4d6f
smorkl <smsalazar0319@gmail.com>
2024-12-05 11:04:36 -0500

    create

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/venv python3
+
+"""
+Declaring variables in Python specifying the data type
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
