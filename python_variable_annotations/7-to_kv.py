f143a841a4ea974fff070bd30622236b299459f6
smorkl <smsalazar0319@gmail.com>
2024-12-05 12:24:09 -0500

    create

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from typing import Union
+
+"""
+This module provides a function to create a tuple from a key-value pair.
+
+The key must be a string, and the value can be either an integer or a float.
+"""
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple:
+    """
+    Creates a tuple from a key-value pair.
+
+    Args:
+        k: The key, a string.
+        v: The value, an integer or a float.
+
+    Returns:
+        A tuple containing the key and value.
+    """
+
+    tuple = (k, v)
+    return tuple
