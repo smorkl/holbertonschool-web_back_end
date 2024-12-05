41484c34f03de7b40924b0c0a01e1e99959c3f14
smorkl <smsalazar0319@gmail.com>
2024-12-04 21:06:29 -0500

    create

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+the module return n floor
+
+fuction:
+    floor(n) Returns the floor of a number.
+"""
+
+
+def floor(n):
+    """
+    Returns the floor of a number.
+
+    arg:
+        n: the number to floor
+
+    Return:
+        The floor of the number.
+    """
+    return n
