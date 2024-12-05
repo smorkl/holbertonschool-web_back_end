2390122e5fcb36e6e4f6157b378adc8cc3ab14f7
smorkl <smsalazar0319@gmail.com>
2024-12-04 20:46:54 -0500

    create

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
-def add (a: float, b: float) -> float:
+"""
+This module provides basic arithmetic operations.
+
+Functions:
+    add(a, b): Adds two floating-point numbers.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two floating-point numbers.
+
+    Args:
+        a: The first number.
+        b: The second number.
+
+    Returns:
+        The sum of a and b.
+    """
     return a + b
