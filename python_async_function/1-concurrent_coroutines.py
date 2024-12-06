c0df1fd9975fd33d5700492943633d3f5186336d
smorkl <smsalazar0319@gmail.com>
2024-12-06 14:51:28 -0500

    create

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,11 +5,10 @@ simple asynchronous call
 wait_random = __import__('0-basic_async_syntax').wait_random
 from typing import List
 
-async def wait_n (n: int, max_delay: int) -> List[float]:
-    i = 0
-    list = []
-    while i <= n:
-        list.append(wait_random(max_delay))
-        i += 1
-    list = sorted(list)
-    return list
+async def wait_n(n, max_delay):
+    delays_list = []
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        delays_list.append(delay)
+
+    return sorted(delays_list)
