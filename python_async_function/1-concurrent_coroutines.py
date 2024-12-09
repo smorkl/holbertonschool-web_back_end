52d9232fc71470f48e8dc65109f04a7527139a7f
smorkl <smsalazar0319@gmail.com>
2024-12-09 11:44:44 -0500

    create

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
 wait_random = __import__("0-basic_async_syntax").wait_random
-from typing import List
+#!/usr/bin/env python3
 """
 simple asynchronous call
 """
+from typing import List
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
