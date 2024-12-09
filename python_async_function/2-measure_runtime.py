09ef48cf2ccd07c8a200736aade4a2ab5d9fcd1f
smorkl <smsalazar0319@gmail.com>
2024-12-09 10:33:37 -0500

    create

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env pyhon3
+"""
+"""
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
+import time
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    start = time.time()
+    await wait_n(n, max_delay)
+    end = time.time()
+    total_time = end - start
+    return total_time / n
