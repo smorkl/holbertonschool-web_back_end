2697bfb9f4254e9deba13e6409e5c2595bdd8fba
smorkl <smsalazar0319@gmail.com>
2024-12-11 10:29:54 -0500

    create

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,6 +4,6 @@
 
 async_generator = __import__("0-async_generator").async_generator
 
-async def async_generator ():
+async def async_comprehension ():
     num_ten = [i async for i in async_generator()] 
     return num_ten
