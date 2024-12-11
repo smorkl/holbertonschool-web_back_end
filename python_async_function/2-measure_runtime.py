3301db52542011501bf064f3813599a91557125b
smorkl <smsalazar0319@gmail.com>
2024-12-10 23:00:29 -0500

    create

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -4,10 +4,9 @@ This module imports the wait_n function
 and executes it n times, and at the end
 we return the average of the time it took to execute.
 """
-
-wait_n = __import__("1-concurrent_coroutines").wait_n
 import time
 import asyncio
+wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
