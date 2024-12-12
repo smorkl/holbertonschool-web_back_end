29452e7e6a31b9566db5249f2f802f8ce4383099
smorkl <smsalazar0319@gmail.com>
2024-12-12 14:39:01 -0500

    create

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -33,7 +33,7 @@ class Server:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
             self.__indexed_dataset = {
-                i: dataset[i] for i in rage(len(dataset))
+                i: dataset[i] for i in range(len(dataset))
                 }
         return self.__indexed_dataset
 
