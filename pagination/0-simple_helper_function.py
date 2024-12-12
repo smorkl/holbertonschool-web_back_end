b37d41518b45521c87febe479dd6f137c9e8e2e1
smorkl <smsalazar0319@gmail.com>
2024-12-12 11:10:25 -0500

    create

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -20,4 +20,4 @@ def index_range(page: int, page_size: int) -> tuple:
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    return (start_index, end_index)
+    return start_index, end_index
