361a6c7e5d23b59fa5a2dea085ae435e2c5951d9
smorkl <smsalazar0319@gmail.com>
2024-12-12 11:07:08 -0500

    create

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,23 @@
+#!/usr/bin/evn python3
+"""
+This module has a function that returns
+the start and end index of a page.
+fuction:
+    index_range(page: int, page_size: int): The function
+    returns the start and end index based on
+    the requested page and the page size
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    The function returns the start and end index based
+    on the requested page and the page size
+    arg:
+        page(int): Requested page number
+        page_size(int): page content size
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
