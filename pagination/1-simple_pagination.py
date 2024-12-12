8f059ad30350cf01082750ecb626e97b38de5cf1
smorkl <smsalazar0319@gmail.com>
2024-12-12 12:11:01 -0500

    create

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -3,6 +3,7 @@ import csv
 import math
 from typing import List
 
+
 def index_range(page: int, page_size: int) -> tuple:
     """
     The function returns the start and end index based
@@ -14,6 +15,7 @@ def index_range(page: int, page_size: int) -> tuple:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return start_index, end_index
+
 
 class Server:
     """Server class to paginate a database of popular baby names."""
@@ -45,10 +47,13 @@ class Server:
         Returns:
             List[List]: The list of rows for the given page.
         """
-        assert isinstance(page, int) and page > 0, "Page must be a positive integer."
-        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer."
+        assert isinstance(page, int) and page > 0, (
+            "Page must be a positive integer."
+        )
+        assert isinstance(page_size, int) and page_size > 0, (
+            "Page size must be a positive integer."
+        )
 
         dataset = self.dataset()
         start, end = index_range(page, page_size)
         return dataset[start:end] if start < len(dataset) else []
-
