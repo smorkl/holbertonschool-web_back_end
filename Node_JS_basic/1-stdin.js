05e2eea6f40ea2835b4dcf40ddb2832b6aeaa8d7
smorkl <smsalazar0319@gmail.com>
2025-01-26 00:22:34 -0500

    create

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,15 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 console.log('Welcome to Holberton School, what is your name?');
-
-rl.on('line', (input) => {
-  process.stdout.write(`Your name is: ${input.trim()}\r\n`);
-  rl.close();
-});
-
-rl.on('close', () => {
-  process.stdout.write('This important software is now closing\r\n');
+// await input
+process.stdin.resume();
+// once input is entered
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+  // check if input is coming from the terminal
+  if (process.stdin.isTTY) {
+    process.exit();
+  } else {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
 });
