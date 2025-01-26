238e6d43883c3c9ab73bc1246b9219597f253aa2
smorkl <smsalazar0319@gmail.com>
2025-01-26 01:21:30 -0500

    create

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,88 +1,58 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
-async function countStudents(filepath) {
-  try {
-    const csv = await fs.promises.readFile(filepath, { encoding: 'utf8' });
-    const headerArray = csv.split(/\r?\n|\n/);
-    const headers = headerArray[0].split(',');
-
-    // strip headers and convert to list of dicts
-    const dictList = [];
-    const noHeaderArray = headerArray.slice(1);
-    for (let i = 0; i < noHeaderArray.length; i += 1) {
-      const data = noHeaderArray[i].split(',');
-      if (data.length === headers.length) {
-        const row = {};
-        for (let j = 0; j < headers.length; j += 1) {
-          row[headers[j].trim()] = data[j].trim();
-        }
-        dictList.push(row);
-      }
-    }
-
-    // count and collect first names of students per field
-    let countCS = 0;
-    let countSWE = 0;
-    const studentsCS = [];
-    const studentsSWE = [];
-
-    dictList.forEach((element) => {
-      if (element.field === 'CS') {
-        countCS += 1;
-        studentsCS.push(element.firstname);
-      } else if (element.field === 'SWE') {
-        countSWE += 1;
-        studentsSWE.push(element.firstname);
-      }
-    });
-
-    const countStudents = countCS + countSWE;
-
-    return ({
-      countStudents,
-      countCS,
-      countSWE,
-      studentsCS,
-      studentsSWE,
-    });
-  } catch (err) {
+const countStudents = (filePath) => {
+  if (!fs.existsSync(filePath)) {
     throw new Error('Cannot load the database');
   }
-}
 
-const pathToDB = process.argv[2];
-const hostname = '127.0.0.1';
-const port = 1245;
+  const data = fs.readFileSync(filePath, 'utf-8');
+  const lines = data.split('\n').filter((line) => line.trim() !== '');
+  const students = lines.slice(1).map((line) => line.split(','));
+
+  const fieldCounts = {};
+
+  students.forEach(([firstname, lastname, age, field]) => {
+    if (!fieldCounts[field]) {
+      fieldCounts[field] = [];
+    }
+    fieldCounts[field].push(firstname);
+  });
+
+  const result = [];
+  result.push(`Number of students: ${students.length}`);
+  for (const [field, names] of Object.entries(fieldCounts)) {
+    result.push(
+      `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`
+    );
+  }
+
+  return result.join('\n');
+};
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    // call async function and collect needed variables
-    countStudents(pathToDB)
-      .then(({
-        countStudents,
-        countCS,
-        countSWE,
-        studentsCS,
-        studentsSWE,
-      }) => {
-        res.write('This is the list of our students\n');
-        res.write(`Number of students: ${countStudents}\n`);
-        res.write(`Number of students in CS: ${countCS}. List: ${studentsCS.toString().split(',').join(', ')}\n`);
-        res.write(`Number of students in SWE: ${countSWE}. List: ${studentsSWE.toString().split(',').join(', ')}`);
-        res.end();
-      })
-      .catch(() => {
-        res.end('Error: Cannot load the database');
-        throw new Error('Cannot load the database');
-      });
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    try {
+      const filePath = path.resolve(__dirname, process.argv[2] || '');
+      const studentInfo = countStudents(filePath);
+      res.end(`This is the list of our students\n${studentInfo}`);
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Cannot load the database');
+    }
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
   }
 });
 
-app.listen(port, hostname);
+app.listen(1245, () => {
+  console.log('Server is running on port 1245');
+});
 
 module.exports = app;
