650ebd0bc9aa6ed798cb09aa42caa5a220103de6
smorkl <smsalazar0319@gmail.com>
2025-01-26 00:20:25 -0500

    create

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -8,10 +8,10 @@ const rl = readline.createInterface({
 console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (input) => {
-  console.log(`Your name is: ${input.trim()}`);
+  process.stdout.write(`Your name is: ${input.trim()}\r\n`);
   rl.close();
 });
 
 rl.on('close', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\r\n');
 });
