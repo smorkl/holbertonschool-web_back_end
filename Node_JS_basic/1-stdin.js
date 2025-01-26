321bd8ec309ec20a3e95929c3df3059e641ecb23
smorkl <smsalazar0319@gmail.com>
2025-01-26 00:17:38 -0500

    create

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,13 +2,13 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
+  console.log(`Your name is: ${input.trim()}`);
   rl.close();
 });
 
