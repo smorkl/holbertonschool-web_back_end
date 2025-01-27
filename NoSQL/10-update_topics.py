0ac6f2e8e14b46d71cd0558d66285a1cf6086542
smorkl <smsalazar0319@gmail.com>
2025-01-26 23:08:04 -0500

    create

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -8,7 +8,7 @@ def update_topics(mongo_collection, name, topics):
     """
     Updates the topics of a school document based on the school name.
     """
-    mongo_collection.update_one(
+    mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
     )
