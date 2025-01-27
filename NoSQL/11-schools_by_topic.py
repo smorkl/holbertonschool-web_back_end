0d7c34d4a96b70b83d6a9f84824b705567e9df69
smorkl <smsalazar0319@gmail.com>
2025-01-26 23:20:04 -0500

    create

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+Module to interact with MongoDB collections.
+"""
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Finds all documents in the collection where the 'topic' field
+    matches the provided topic.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The MongoDB
+        collection object to search in.
+        topic (str): The topic to search for in the 'topic' field of
+        the documents.
+
+    Returns:
+        list: A list of documents where the 'topic'
+        field matches the provided topic.
+    """
+    query = {"topic": topic}
+    result = mongo_collection.find(query)
+
+    return list(result)
