1939ac9275e347c26240b664d2fd952b6415f583
smorkl <smsalazar0319@gmail.com>
2025-01-26 23:10:36 -0500

    create

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 """
-FUNCTION update_topics(mongo_collection, name, topics)
+This module contains a function to update the 'topics' field in all documents
+within a MongoDB collection that match a given school name.
+
+Function:
+    update_topics(mongo_collection, name, topics)
+        Updates the 'topics' field of documents that match the given 'name' in
+        the MongoDB collection.
+
+Parameters:
+    mongo_collection (pymongo.collection.Collection): The MongoDB collection
+    name (str): The name of the school to search for in the collection
+    topics (list of str): A list of topics to set for the specified school
 """
 
 def update_topics(mongo_collection, name, topics):
