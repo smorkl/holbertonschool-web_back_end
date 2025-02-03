32888066e3b217922aaa6b9afcdf220e6b23a654
smorkl <smsalazar0319@gmail.com>
2025-02-03 10:30:47 -0500

    create

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,14 @@
-console.log('Welcome to Holberton School, what is your name?');
-
-// Leer entrada estándar
-process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (chunk) => {
-  const name = chunk.trim(); // Remover saltos de línea y espacios adicionales
-  console.log(`Your name is: ${name}`);
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name != null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.on('exit', () => {
   console.log('This important software is now closing');
-  process.exit(0); // Salida exitosa
 });
